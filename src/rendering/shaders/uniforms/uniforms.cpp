2b8762a94a0aedc27815c1fc17310492d22213f1
wortelus <daniel.slavik.cz@gmail.com>
2023-11-05 01:59:36 +0100

    Small fixes

--- src/rendering/shaders/uniforms/uniforms.cpp
+++ src/rendering/shaders/uniforms/uniforms.cpp
@@ -7,27 +7,39 @@
 
 
 void Uniforms::passUniform1i(GLint location, GLint value) {
+    if (location == -1)
+        return;
     glUniform1i(location, value);
 }
 
 void Uniforms::passUniform1f(GLint location, GLfloat value) {
+    if (location == -1)
+        return;
     glUniform1f(location, value);
 }
 
 void Uniforms::passUniform3fv(GLint location, const glm::vec3& value) {
+    if (location == -1)
+        return;
     glUniform3fv(location, 1, glm::value_ptr(value));
 }
 
 void Uniforms::passUniform4fv(GLint location, const glm::vec4& value) {
+    if (location == -1)
+        return;
     glUniform4fv(location, 1, glm::value_ptr(value));
 
 }
 
 void Uniforms::passUniformMatrix3fv(GLint location, const glm::mat3& value) {
+    if (location == -1)
+        return;
     glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(value));
 }
 
 void Uniforms::passUniformMatrix4fv(GLint location, const glm::mat4& value) {
+    if (location == -1)
+        return;
     glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(value));
 }
 
